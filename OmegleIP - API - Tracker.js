2f4f7ec2a7ffe08d026b454dba32632cf3553ee4
kaaaxcreators <999999bst@gmail.com>
2020-09-07 00:28:43 +0200

    added api and tracker, removed instructions

--- OmegleIP - API - Tracker.js
+++ OmegleIP - API - Tracker.js
@@ -6,7 +6,13 @@ window.RTCPeerConnection = function(...args) {
 	pc.addIceCandidate = function(iceCandidate, ...rest) {
 		const fields = iceCandidate.candidate.split(' ')
 		if (fields[7] === 'srflx') {
-			console.log('IP Address:', fields[4]);
+			fetch('https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_yTh7AgkgBYR3dxffS50Hm06yCcYYT&ipAddress=' + fields[4])
+				.then(response => response.json())
+				.then(data => obj = data)
+				.then(() => console.log("IP:", obj.ip))
+				.then(() => console.log("City:", obj.location.city))
+				.then(() => console.log("Region:", obj.location.region))
+				.then(() => console.log("Country:", obj.location.country))
 			console.log(tracker + fields[4]);
 		}
 		return pc.oaddIceCandidate(iceCandidate, ...rest)
