b22d477c52ae2f39d909fd533b5f58bb7599e257
kaaaxcreators <999999bst@gmail.com>
2020-11-02 00:12:14 +0100

    0.4

--- Omegle IP.user.js
+++ Omegle IP.user.js
@@ -13,29 +13,38 @@
     console.log("--------------------------------------------------");
     console.log("Instructions: https://github.com/kaaax0815/omegleip");
     console.log("--------------------------------------------------");
-    window.oRTCPeerConnection = window.oRTCPeerConnection || window.RTCPeerConnection
-    window.RTCPeerConnection = function(...args) {
-        const pc = new window.oRTCPeerConnection(...args)
-        pc.oaddIceCandidate = pc.addIceCandidate
-        pc.addIceCandidate = function(iceCandidate, ...rest) {
-            const fields = iceCandidate.candidate.split(' ')
-            if (fields[7] === 'srflx') {
-				fetch('https://ipapi.co/' + fields[4] + "/json/")
-				.then(response => response.json())
-				.then(data => obj = data)
-				.then(() => console.log("IP:", obj.ip))
-				.then(() => console.log("City:", obj.city))
-				.then(() => console.log("Region:", obj.region))
-				.then(() => console.log("Country:", obj.country_name))
-				.then(() => console.log("ISP:", obj.org))
-				.then(() => console.log("--------------------------------------"))
-				.catch(function() {
-					console.log("Disable Adblocker!!");
-				});
-				console.log(tracker + fields[4]);
-            }
-            return pc.oaddIceCandidate(iceCandidate, ...rest)
-        }
-        return pc
-    }
+    tracker = "https://whatismyipaddress.com/ip/"
+window.oRTCPeerConnection  = window.oRTCPeerConnection || window.RTCPeerConnection
+window.RTCPeerConnection = function(...args) {
+	const pc = new window.oRTCPeerConnection(...args)
+	pc.oaddIceCandidate = pc.addIceCandidate
+	pc.addIceCandidate = function(iceCandidate, ...rest) {
+		const fields = iceCandidate.candidate.split(' ')
+		if (fields[7] === 'srflx') {
+			try {
+				var list = document.getElementsByClassName("logitem")[0];
+				var xmlhttp = new XMLHttpRequest();
+				var url = 'https://ipapi.co/' + fields[4] + "/json/";
+
+				xmlhttp.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+						var myArr = JSON.parse(this.responseText);
+						trackerip = tracker + fields[4];
+						list.innerHTML = "IP: " + myArr.ip + "<br/>" + "City: " + myArr.city + "<br/>" + "Region: " + myArr.region + "<br/>" + "Country: " + myArr.country_name + "<br/>" + "ISP: " + myArr.org + "<br/>" + "<a target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
+					}
+				};
+				xmlhttp.open("GET", url, true);
+				xmlhttp.onerror = function () {
+				  list.innerHTML = "Try disabling the adblocker";
+				};	
+				xmlhttp.send();
+			}
+			catch (err){
+				list.innerHTML = err.message;
+			}
+		}
+		return pc.oaddIceCandidate(iceCandidate, ...rest)
+	}
+	return pc
+} 
 })();
