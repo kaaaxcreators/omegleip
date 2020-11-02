e3e793bd6d82911cc863221be78455c89d2a850f
kaaaxcreators <999999bst@gmail.com>
2020-11-02 23:01:44 +0100

    prints link for information over tracker even if you reached your daily quota

--- OmegleIP - API - Tracker - NoConsole.js
+++ OmegleIP - API - Tracker - NoConsole.js
@@ -10,18 +10,18 @@ window.RTCPeerConnection = function(...args) {
 				var list = document.getElementsByClassName("logitem")[0];
 				var xmlhttp = new XMLHttpRequest();
 				var url = 'https://ipapi.co/' + fields[4] + "/json/";
+				trackerip = tracker + fields[4];
 
 				xmlhttp.onreadystatechange = function() {
 					if (this.readyState == 4 && this.status == 200) {
 						var myArr = JSON.parse(this.responseText);
-						trackerip = tracker + fields[4];
-						list.innerHTML = "IP: " + myArr.ip + "<br/>" + "City: " + myArr.city + "<br/>" + "Region: " + myArr.region + "<br/>" + "Country: " + myArr.country_name + "<br/>" + "ISP: " + myArr.org + "<br/>" + "<a target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
+						list.innerHTML = "IP: " + myArr.ip + "<br/>" + "City: " + myArr.city + "<br/>" + "Region: " + myArr.region + "<br/>" + "Country: " + myArr.country_name + "<br/>" + "ISP: " + myArr.org + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
 					}
 					if (this.status > 399 && this.status < 600) {
-						list.innerHTML = "An error occured. (HTTP Statuscode: " + this.status + ")";
+						list.innerHTML = "An error occured. (HTTP Statuscode: " + this.status + ")" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
 					}
 					if (this.status === 429) {
-						list.innerHTML = "You exceeded your daily quota. (429 Too Many Requests)";
+						list.innerHTML = "You exceeded your daily quota. (429 Too Many Requests)" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
 					}
 				};
 				xmlhttp.open("GET", url, true);
