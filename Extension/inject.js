ff6087fd33243af7e7b5987b6d51d7145920a5d4
kaaax0815 <999999bst@gmail.com>
2021-02-16 19:00:39 +0100

    fixed errors

--- Extension/inject.js
+++ Extension/inject.js
@@ -35,7 +35,7 @@ function getIp(tracker, trollChecked, api_key){
 	console.log("Tracker:", tracker);
 	console.log("Troll?:", trollChecked);
 	if (api_key == "random") {
-		api_list = ["78aa4e8e374d134f0a29b3fb2c057e7d0a179517bd52cf868a5ceb51","8b1d695c1a9a68a426b1c6afb0925e41f5244ed93b2cdfc8bf448f26","9395e94ad6dceab9bd0a7e4ffc48340305cede433dead3e66d8f015e"]
+		api_list = ["78aa4e8e374d134f0a29b3fb2c057e7d0a179517bd52cf868a5ceb51","8b1d695c1a9a68a426b1c6afb0925e41f5244ed93b2cdfc8bf448f26","9395e94ad6dceab9bd0a7e4ffc48340305cede433dead3e66d8f015e", "13c42906bdb22e8ddab6f4fcb20e2a8563317deda9d71b77080f43e3"]
 		api_key = api_list[Math.floor(Math.random() * api_list.length)];
 	}
   	console.log("API-Key:", api_key);
@@ -74,19 +74,46 @@ function getIp(tracker, trollChecked, api_key){
 										var fallback = JSON.parse(this.responseText); // gets the reponse as json objects
 										ip = fallback.ip; city = fallback.city; region = fallback.region; country =  fallback.country_name; isp = fallback.asn.name;
 										if (trollChecked) { // checks if the troll button checkbox is checked in the options
-											list.innerHTML = "Using Fallback API:" + "<br/>" + "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>" + "<br/>" + "<button style=\"background-color:white; text cursor:pointer\" onclick=\"sendStranger(ip, city, region, country, isp)\">Send Infos to Stranger</button>";
+											list.innerHTML = "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>" + "<br/>" + "<button style=\"background-color:white; text cursor:pointer\" onclick=\"sendStranger(ip, city, region, country, isp)\">Send Infos to Stranger</button>";
 										}
 										else {
-											list.innerHTML = "Using Fallback API:" + "<br/>" + "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
+											list.innerHTML = "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
 										}
 									}
-									if (this.status > 399 && this.status < 600) { // error handling on html error 400 - 599
-										list.innerHTML = "An error occured. (HTTP Statuscode: " + this.status + ")" + "<br/>" + "Reloading the Page may work" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
-
-									}
-									if (this.status === 429) { // // error handling on html error 429
-										list.innerHTML = "You exceeded your daily quota. (429 Too Many Requests)" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
-
+									if (this.status > 299 && this.status < 600) { // error handling on html error 300 - 599 -> use fallback api
+										try {
+											var xmlhttp = new XMLHttpRequest(); // creates xmlhttprequest to the api to get geolocation
+											var fall2backapi = 'https://ipwhois.app/json/' + fields[4]; // sets fallback api
+			
+											xmlhttp.onreadystatechange = function() {
+												if (this.readyState == 4 && this.status == 200) { // if succeeds
+													var fall2back = JSON.parse(this.responseText); // gets the reponse as json objects
+													ip = fall2back.ip; city = fall2back.city; region = fall2back.region; country =  fall2back.country; isp = fall2back.isp;
+													if (trollChecked) { // checks if the troll button checkbox is checked in the options
+														list.innerHTML = "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>" + "<br/>" + "<button style=\"background-color:white; text cursor:pointer\" onclick=\"sendStranger(ip, city, region, country, isp)\">Send Infos to Stranger</button>";
+													}
+													else {
+														list.innerHTML = "IP: " + ip + "<br/>" + "City: " + city + "<br/>" + "Region: " + region + "<br/>" + "Country: " + country + "<br/>" + "ISP: " + isp + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "More Information" + "</a>";
+													}
+												}
+												if (this.status > 399 && this.status < 600) { // error handling on html error 400 - 599
+													list.innerHTML = "An error occured. (HTTP Statuscode: " + this.status + ")" + "<br/>" + "Reloading the Page may work" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
+			
+												}
+												if (this.status === 429) { // // error handling on html error 429
+													list.innerHTML = "You exceeded your daily quota. (429 Too Many Requests)" + "<br/>" + "<a style=\"color:black;\" target =\"_blank\" href=\"" + trackerip + "\">" + "External Information about " + fields[4] + "</a>";
+			
+												}
+											};
+											xmlhttp.open("GET", fall2backapi, true);
+											xmlhttp.onerror = function () {
+											  list.innerHTML = "Try disabling the adblocker";
+											};
+											xmlhttp.send(); // send the request
+										}
+										catch (err){
+											list.innerHTML = "A Error occurred: " + err.message;
+										}
 									}
 								};
 								xmlhttp.open("GET", fallbackapi, true);
@@ -120,7 +147,6 @@ function sendStranger(ip, city, region, country, isp) { // send stranger own det
 	chat.value = "IP: " + ip + "\nCity: " + city + "\nRegion: " + region + "\nCountry: " + country + "\nISP: " + isp;
 	var button = document.getElementsByClassName("sendbtn")[0];
 	button.click();
-	instance.action('0a3ae684-f3cd-4ce8-b171-fac30f1f51b9', { key: 'Click', value: '1' }) // Ackee Analytics
 	return;
 }
 
